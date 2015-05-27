272fe52f3b5b35ce0c44853d510b0d0760279b19
Wonderfall <wonderaccounts@openmailbox.org>
2015-05-27 18:26:54 +0200

    Many changes.
    Cleaner code. Daily tweets remover. More functions.

--- streamer.py
+++ streamer.py
@@ -1,24 +1,47 @@
 #!/usr/bin/env python
 
-# REQUIRED
+# REQUIRED IMPORTS
 import time
 import sys
 import psutil
 import subprocess
+import shutil
+import os
+import random
+from apscheduler.scheduler import Scheduler # Must be apscheduler 2.1.2 !
 from twython import TwythonStreamer
 from twython import Twython
+from twython import TwythonError
 from datetime import timedelta
 
-# Twitte Authentification
+# Twitter Credentials & accounts
 apiKey = 'foobar'
 apiSecret = 'foobar'
 accessToken = 'foobar'
 accessTokenSecret = 'foobar'
+bot, master = 'foobar', 'foobar'
 api = Twython(apiKey,apiSecret,accessToken,accessTokenSecret)
 
+# Path variables
+log = 'foobar'
+torrentsCompleted = 'foobar'
+torrentsProgress = 'foobar'
+owncloudTorrents = 'foobar'
+
+# Scheduler : daily tweets remover
+sched = Scheduler()
+
+@sched.interval_schedule(hours=24)
+def delete_tweets():
+        user_timeline = api.get_user_timeline(screen_name=bot, count=100)
+        for tweets in user_timeline:
+                api.destroy_status(id=tweets['id_str'])
+
+sched.start()
+
 # Stream filter
-TERMS = 'foobar'
-FOLLOW = 'foobar'
+FOLLOW = api.show_user(screen_name=master)
+TERMS = '@' + bot
 
 # Functions
 def giveUptime() :
@@ -39,30 +62,106 @@ def speedtest() :
     output = subprocess.check_output(["speedtest-cli", "--simple"])
     return output
 
+def checkMalwares() :
+    output1 = subprocess.check_output(["grep", "Possible", "/var/log/rkhunter.log"])
+    output2 = subprocess.check_output(["grep", "Suspect", "/var/log/rkhunter.log"])
+    if (output1[30] == '0') and (output2[26] == '0') :
+        output = "No rootkits, no supect files. Nothing to worry about."
+    else :
+        output = "A son of bitch is annoying me. Check me as soon as you can."
+    return output
+
+def movetree(src, dst, deleteSrc, symlinks=False, ignore=None):
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.exists(d):
+            try:
+                shutil.rmtree(d)
+            except Exception as e:
+                os.unlink(d)
+        if os.path.isdir(s):
+            shutil.copytree(s, d, symlinks, ignore)
+        else:
+            shutil.copy2(s, d)
+    if deleteSrc == True :
+        shutil.rmtree(src)
+
+def numberFiles(src):
+    filesListed = len(os.listdir(src))
+    return filesListed
+
+def updateWithRandom(output):
+    output = output + '\n' + '#' + str(random.randint(1,9999))
+    api.update_status(status=output)
+
+# Answer function
 def answer(tweet) :
+
     if 'toast' in tweet:
-        api.update_status(status="Tell me what I've to do...")
+        updateWithRandom("Tell me what I've to do...")
+
     elif 'uptime' in tweet:
         api.update_status(status=giveUptime())
-    elif 'CPUload' in tweet:
-        api.update_status(status=giveCPU())
-    elif 'RAMusage' in tweet:
-        api.update_status(status=giveRAM())
+
+    elif ('CPU' in tweet) or ('load' in tweet):
+        updateWithRandom(giveCPU())
+
+    elif ('RAM' in tweet) or ('memory' in tweet):
+        updateWithRandom(giveRAM())
+
     elif 'speedtest' in tweet:
         api.update_status(status=speedtest())
+
+    elif ('malwares' in tweet) or ('rootkits' in tweet):
+        updateWithRandom(checkMalwares())
+
+    elif ('torrents' in tweet) and (('check' in tweet) or ('copy' in tweet) or ('move' in tweet) or ('clean' in tweet) or ('save' in tweet)) :
+
+        if 'check' in tweet: 
+            inProgress = numberFiles(torrentsProgress)
+            checkedTorrents = ' ' + str(inProgress) + ' torrent(s) are in progress.'
+            whatDone = ' checked '
+        elif ('copy' in tweet) or ('save' in tweet):
+            movetree(torrentsCompleted, owncloudTorrents, False)
+            whatDone, checkedTorrents = ' copied ', ''
+        elif ('move' in tweet) or ('clean' in tweet):
+            movetree(torrentsCompleted, owncloudTorrents, True)
+            whatDone, checkedTorrents = ' moved ', ''
+
+        outputTorrents = 'Torrents have been' + whatDone + ', sir.' + checkedTorrents
+        updateWithRandom(outputTorrents)
+
     else :
-        api.update_status(status="I'm afraid I don't understand...")
+        updateWithRandom("I'm afraid I don't understand...")
 
 # Streamer
 class TweetStreamer(TwythonStreamer):
     def on_success(self, data):
-        if 'text' in data:
-            print data['text'].encode('utf-8')
+        if ('text' in data) and (TERMS in data['text']):
+            f = open(log, 'a')
+            infoLog = 'Received at ' + datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S') + ' : ' + data['text'] + '\n'
+            f.write(infoLog)
+            f.close()
+            #print data['text'].encode('utf-8')
             answer(data['text'])
 
     def on_error(self, status_code, data):
-        print status_code
+        f = open(log, 'a')
+        errorLog2 = 'Error at ' + datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S') + ' : ' + status_code + '\n'
+        f.write(errorLog)
+        f.close()
+        #print status_code
         self.disconnect()
 
 stream = TweetStreamer(apiKey,apiSecret,accessToken,accessTokenSecret)
-stream.statuses.filter(tracks=TERMS, follow=FOLLOW)
+
+try :
+    stream.statuses.filter(follow=FOLLOW)
+except TwythonError as e :
+    f = open(log, 'a')
+    errorLog = 'Error at ' + datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S') + ' : ' + e
+    f.write(errorLog)
+    f.write('\n Valar Morghulis. \n \n \n')
+    f.close()
+    #print e
