ec55d5c5d5c734ab28208473588823d67ef9f7f0
Wonderfall <wonderaccounts@openmailbox.org>
2015-05-30 01:43:52 +0200

    Better code.
    Cleaner variable, log function, associative arrow instead of if/elif
    statement, etc.

--- streamer.py
+++ streamer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# REQUIRED IMPORTS
+### REQUIRED IMPORTS
 import time
 import sys
 import psutil
@@ -13,28 +13,28 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from twython import Twython, TwythonError, TwythonStreamer
 
 #### Twitter Credentials & accounts
-apiKey            = 'foobar'
-apiSecret         = 'foobar'
-accessToken       = 'foobar'
-accessTokenSecret = 'foobar'
-bot, master       = 'foobar', 'foobar'
-api               = Twython(apiKey,apiSecret,accessToken,accessTokenSecret)
+API_KEY             = 'foobar'
+API_SECRET          = 'foobar'
+ACCESS_TOKEN        = 'foobar'
+ACCESS_TOKEN_SECRET = 'foobar'
+BOT, MASTER         = 'foobar', 'foobar'
+api                 = Twython(API_KEY,API_SECRET,ACCESS_TOKEN,ACCESS_TOKEN_SECRET)
 
 #### Path variables
-log               = 'foobar'
-torrentsCompleted = 'foobar'
-torrentsProgress  = 'foobar'
-owncloudTorrents  = 'foobar'
-rkhunterLog       = 'foobar'
+LOG                = 'foobar'
+TORRENTS_COMPLETED = 'foobar'
+TORRENTS_PROGRESS  = 'foobar'
+OWNCLOUD_TORRENTS  = 'foobar'
+RKHUNTER_LOG       = 'foobar'
 
 #### Scheduler : daily tweets & log remover
 def delete_tweets():
-    user_timeline = api.get_user_timeline(screen_name=bot, count=100)
+    user_timeline = api.get_user_timeline(screen_name=BOT, count=100)
     for tweets in user_timeline:
         api.destroy_status(id=tweets['id_str'])
 
 def delete_log():
-    f = open(log, 'w')
+    f = open(LOG, 'w')
     f.close()
 
 scheduler = BackgroundScheduler()
@@ -43,38 +43,43 @@ scheduler.add_job(delete_log, 'interval', hours=24)
 scheduler.start() # Comment it if you don't want to use it
 
 #### Stream filter
-FOLLOW = str(int(api.show_user(screen_name=master)['id_str']))
-TERMS = '@' + bot
+FOLLOW = str(int(api.show_user(screen_name=MASTER)['id_str']))
+TERMS = '@' + BOT
 
 #### Functions
-def giveUptime() :
+def append_log(to_write):
+    f = open(LOG, 'a')
+    f.write('At ' + str(datetime.now()) + ' : ' + to_write + '\n')
+    f.close()
+
+def tweet_random(output=None):
+    if output == None: output = 'Roah. Tell me something understable.'
+    api.update_status(status=output + '\n' + '#' + str(random.randint(1,9999)))
+
+def give_uptime() :
     with open('/proc/uptime', 'r') as f:
         uptime_seconds = float(f.readline().split()[0])
         uptime_string = "Uptime : " + str(timedelta(seconds = uptime_seconds))
-    return uptime_string
+    tweet_random(uptime_string)
 
-def giveCPU() :
-    cpuLoad = "CPU load : " + str(psutil.cpu_percent(interval=1)) + " %"
-    return cpuLoad
+def give_cpuload() :
+    tweet_random("CPU load : " + str(psutil.cpu_percent(interval=1)) + " %")
 
-def giveRAM() :
-    RAMusage = "RAM usage : " + str(psutil.phymem_usage().percent) + " %"
-    return RAMusage
+def give_RAMusage() :
+    tweet_random("RAM usage : " + str(psutil.phymem_usage().percent) + " %")
 
 def speedtest() :
-    output = subprocess.check_output(["speedtest-cli", "--simple"])
-    return output
+    tweet_random(subprocess.check_output(["speedtest-cli", "--simple"]).decode('utf-8'))
 
-def checkMalwares() :
-    output1 = subprocess.check_output(["grep", "Possible", rkhunterLog])
-    output2 = subprocess.check_output(["grep", "Suspect", rkhunterLog])
-    if (output1[30] == '0') and (output2[26] == '0') :
-        output = "No rootkits, no supect files. Nothing to worry about."
-    else :
-        output = "A son of bitch is annoying me. Check me as soon as you can."
-    return output
+def check_malwares() :
+    check1 = subprocess.check_output(["grep", "Possible", RKHUNTER_LOG]).decode('utf-8')
+    check2 = subprocess.check_output(["grep", "Suspect", RKHUNTER_LOG]).decode('utf-8')
+    if (check1[30] == '0') and (check2[26] == '0'):
+        tweet_random("No rootkits, no supect files. Nothing to worry about.")
+    else:
+        tweet_random("A son of bitch is annoying me. Check me as soon as you can.")
 
-def movetree(src, dst, deleteSrc, symlinks=False, ignore=None):
+def move_torrents(src=TORRENTS_COMPLETED, dst=OWNCLOUD_TORRENTS, symlinks=False, ignore=None):
     for item in os.listdir(src):
         s = os.path.join(src, item)
         d = os.path.join(dst, item)
@@ -87,81 +92,47 @@ def movetree(src, dst, deleteSrc, symlinks=False, ignore=None):
             shutil.copytree(s, d, symlinks, ignore)
         else:
             shutil.copy2(s, d)
-    if deleteSrc == True :
-        shutil.rmtree(src)
+    shutil.rmtree(src)
+    tweet_random('Torrent(s) moved, sir. Being synchronized.')
 
-def numberFiles(src):
-    filesListed = len(os.listdir(src))
-    return filesListed
+def downloading_torrents(src=TORRENTS_PROGRESS):
+    tweet_random(str(len(os.listdir(src))) + ' torrent(s) are downloading, sir.')
 
-def updateWithRandom(output):
-    output = output + '\n' + '#' + str(random.randint(1,9999))
-    api.update_status(status=output)
+#### Associated words
+options = {
+    'toast'         : tweet_random,
+    'uptime'        : give_uptime,
+    'CPU'           : give_cpuload,
+    'RAM'           : give_RAMusage,
+    'speedtest'     : speedtest,
+    'malwares'      : check_malwares,
+    'moveTorrents'  : move_torrents,
+    'checkTorrents' : downloading_torrents
+}
+
+words_options = options.keys()
 
 #### Answer function
-def answer(tweet) :
-
-    if 'toast' in tweet:
-        updateWithRandom("Tell me what I've to do...")
-
-    elif 'uptime' in tweet:
-        api.update_status(status=giveUptime())
-
-    elif ('CPU' in tweet) or ('load' in tweet):
-        updateWithRandom(giveCPU())
-
-    elif ('RAM' in tweet) or ('memory' in tweet):
-        updateWithRandom(giveRAM())
-
-    elif 'speedtest' in tweet:
-        api.update_status(status=speedtest())
-
-    elif ('malwares' in tweet) or ('rootkits' in tweet):
-        updateWithRandom(checkMalwares())
-
-    elif ('torrents' in tweet) and (('check' in tweet) or ('copy' in tweet) or ('move' in tweet) or ('clean' in tweet) or ('save' in tweet)) :
-
-        if 'check' in tweet: 
-            inProgress = numberFiles(torrentsProgress)
-            checkedTorrents = ' ' + str(inProgress) + ' torrent(s) are in progress.'
-            whatDone = ' checked'
-        elif ('copy' in tweet) or ('save' in tweet):
-            movetree(torrentsCompleted, owncloudTorrents, False)
-            whatDone, checkedTorrents = ' copied', ''
-        elif ('move' in tweet) or ('clean' in tweet):
-            movetree(torrentsCompleted, owncloudTorrents, True)
-            whatDone, checkedTorrents = ' moved', ''
-
-        outputTorrents = 'Torrents have been' + whatDone + ', sir.' + checkedTorrents
-        updateWithRandom(outputTorrents)
-
-    else :
-        updateWithRandom("I'm afraid I don't understand...")
+def answer(tweet):
+    splited_tweet = tweet.split()
+    for i in range(len(splited_tweet)):
+        if splited_tweet[i] in words_options:
+            options[splited_tweet[i]]()
+            break
 
 #### Streamer
 class TweetStreamer(TwythonStreamer):
     def on_success(self, data):
         if ('text' in data) and (TERMS in data['text']):
-            f = open(log, 'a')
-            infoLog = 'Received at ' + str(datetime.now()) + ' : ' + data['text'] + '\n'
-            f.write(infoLog)
-            f.close()
+            append_log('tweet from ' + MASTER + ' to ' + BOT + ' received.')
             answer(data['text'])
 
     def on_error(self, status_code, data):
-        f = open(log, 'a')
-        errorLog2 = 'Error at ' + str(datetime.now()) + ' : ' + status_code + '\n'
-        f.write(errorLog2)
-        f.close()
         self.disconnect()
 
-stream = TweetStreamer(apiKey,apiSecret,accessToken,accessTokenSecret)
+stream = TweetStreamer(API_KEY,API_SECRET,ACCESS_TOKEN,ACCESS_TOKEN_SECRET)
 
-try :
+try:
     stream.statuses.filter(follow=FOLLOW)
-except TwythonError as e :
-    f = open(log, 'a')
-    errorLog = 'Error at ' + str(datetime.now()) + ' : ' + str(e)
-    f.write(errorLog)
-    f.write('\n Valar Morghulis. \n \n \n')
-    f.close()
+except TwythonError as e:
+    append_log(str(e))
